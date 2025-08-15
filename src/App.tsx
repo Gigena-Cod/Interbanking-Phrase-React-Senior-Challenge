f6df491e9d353e854d3f7135618f1d53ac37fca4
Christian Gigena <nahuel.gigena@hotmail.com>
2025-08-15 12:16:51 -0300

    MODIFY: Changes in general

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import "./App.css";
 import { ViewPhrases } from "./infrastructure/features/ViewPhrases/ViewPhrases ";
-
+import { Provider } from "react-redux";
+import { store } from "./infrastructure/redux";
+import { ToastContainer } from "react-toastify";
 function App() {
-  return <ViewPhrases />;
+  return (
+    <Provider store={store}>
+      <ViewPhrases />
+      <ToastContainer />
+    </Provider>
+  );
 }
 
 export default App;
